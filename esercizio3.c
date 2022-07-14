63212f1bf47b6960e10853a1d93bd015ed57b6b7
SimoneCannizzaro <109146607+SimoneCannizzaro@users.noreply.github.com>
2022-07-14 13:09:56 +0200

    Add files via upload

--- esercizio3.c
+++ esercizio3.c
@@ -3,25 +3,25 @@
 int main ()
 {
 /* variabili */
-   
+
    int num ;
 /* inserimento numero */
 
    printf("inserisci il tuo numero \n");
    scanf("%d",& num);
 /* funzione per vedere se e' negativo o positivo */
-   
-   if(num>0)
-   
+
+/* diamo per ipotesi che 0 sia positivo */
+
+   if(num>=0)
+
    printf(" Il tuo numero e' positivo \n");
-   
+
    else
 /* output risultato */
-   
+
    printf("Il tuo numero e' negativo \n");
-   
+
    return 0;
 
 }
-   
-   
